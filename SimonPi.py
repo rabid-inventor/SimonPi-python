e87b6580c224df9704debca4b6aa9212ef04f100
Rabid Inventor <jon@pimoroni.com>
2014-08-26 16:03:57 +0000

    More code

--- SimonPi.py
+++ SimonPi.py
@@ -20,26 +20,23 @@ import threading
 import RPi.GPIO as GPIO   
 from time import sleep as delay
 
+
 sck = socket(AF_INET, SOCK_STREAM)
 
+gameActive = False
 
-#sck.connect(("127.0.0.1",3000))
 
-os.system("pd -nogui tonegenerator.pd &")
+print(os.system("pd -nogui tonegenerator.pd &"))
 print('now you wait ...........')
-delay(0.5)
+delay(1.5)
 
 sck.connect(("127.0.0.1",3000))
 
-sck.send("power 1;")
-
-
+#sck.send("power 1;")
 #os.system('echo "power 1;" | pdsend 3000')
-delay(1)
-os.system('echo "power 0;" | pdsend 3000') 
-
-os.system('sudo killall pd') 
-exit()
+#delay(1)
+#os.system('echo "power 0;" | pdsend 3000') 
+#os.system('sudo killall pd') 
 
 outputs = [22,23,24,25]
 light_speed = 0.1
@@ -53,11 +50,15 @@ print(Notes.get('aSH'))
 Tune1 = [('a',500),('aSH',500)]
 
 def playNote((f,d)):
-   command = "speaker-test -t sine  -l 1 -f " + str(f) + " -p " + str(d)
-   print(command)
-   os.system(command)
+   sck.send("tone  " + str(f) + ";")
+   sck.send("power 1;")
+   delay(d)
+   sck.send("power 0;")
+
+
 
 #playNote((440,1))
+#playNote((330,1))
 
 def printFunction(channel):
    print("Button 1 pressed!")
@@ -65,14 +66,52 @@ def printFunction(channel):
 
 
 
+def button1(channel):
+   print channel
+   print("button 1 pressed")
+   playNote((440,0.4))
+   return
+
+def button2(channel):
+   print("button 2 pressed")
+   playNote((330,0.4))
+   return
+
+
+def button3(channel): 
+   gameActive = True
+   fail()
+
+
+def fail():
+   playNote((220,3))
+   gameActive = True
+   #lights(0.2)
+
+
+
 for i in range(len(outputs)):
    #GPIO.setup(outputs[i],GPIO.OUT)
    GPIO.setup(outputs[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-   GPIO.add_event_detect(outputs[i], GPIO.FALLING, callback=printFunction, bouncetime=300)   
+   
 #GPIO.output(22,True)
    
+def attachInt(button,IOport):
+   if ( button == 0):
+      GPIO.add_event_detect(IOport, GPIO.FALLING, callback=button1, bouncetime=300)
+   elif( button == 1):
+      GPIO.add_event_detect(IOport, GPIO.FALLING, callback=button2, bouncetime=300)
+   elif( button == 2):
+      GPIO.add_event_detect(IOport, GPIO.FALLING, callback=button3, bouncetime=300)
+   elif( button == 3):
+      GPIO.add_event_detect(IOport, GPIO.FALLING, callback=button2, bouncetime=300)
+   return 0
+
+
+
 def lights(speed):
-   while 1:
+  
+   while (gameActive == False):
       for i in range(len(outputs)):
 
          GPIO.remove_event_detect(outputs[i])
@@ -80,11 +119,9 @@ def lights(speed):
          GPIO.output(outputs[i], True)
          delay(speed)
          GPIO.setup(outputs[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-         GPIO.add_event_detect(outputs[i], GPIO.FALLING, callback=printFunction, bouncetime=300)
+         attachInt(i,outputs[i]) 
          delay(speed)   
 
-
-#GPIO.add_event_detect(23, GPIO.FALLING, callback=printFunction, bouncetime=300)
 t = threading.Thread(target = lights, args = [light_speed])
 t.deamon = False
 t.start()
@@ -104,5 +141,9 @@ while 1:
       
    except KeyboardInterrupt:
         #t.stop()
+        os.system('sudo killall pd')
         GPIO.cleanup()
+        
+        os.system('sudo killall pd')
+
         break
